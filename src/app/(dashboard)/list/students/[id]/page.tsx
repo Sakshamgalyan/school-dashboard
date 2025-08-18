7a064d5a02befd03bb4e1be8e2bb08c8c4c99a48
Saksham Galyan <146658307+Sakshamgalyan@users.noreply.github.com>
2025-08-18 22:10:46 +0530

    sign in

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -79,7 +79,7 @@ const SingleStudentPage = () => {
                 </div>
                 {/* bottom */}
                 <div className="mt-4  bg-white rounded-md p-4 h-[800px]">
-                    <h1>Teacher's Schedule</h1>
+                    <h1>Teacher&apos;s Schedule</h1>
                     <BigCalendar />
                 </div>
             </div>
@@ -88,11 +88,11 @@ const SingleStudentPage = () => {
                 <div className="bg-white p-4 rounded-md">
                     <h1 className="text-xl font-semibold">Shortcuts</h1>
                     <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-                        <Link className="p-3 rounded-md bg-SKlightsky" href={`/list/lessons?classId=${2}`}>Student's Lessons</Link>
-                        <Link className="p-3 rounded-md bg-SKlightpurple" href={`/list/teachers?classId=${2}`}>Student's Teachers</Link>
-                        <Link className="p-3 rounded-md bg-pink-50" href={`/list/exams?classId=${2}`}>Student's Exams</Link>
-                        <Link className="p-3 rounded-md bg-SKlightsky" href={`/list/assignments?classId=${2}`}>Student's Assignments</Link>
-                        <Link className="p-3 rounded-md bg-SKlightyellow" href={`/list/results?studentId=${"student2"}`}>Student's Results</Link>
+                        <Link className="p-3 rounded-md bg-SKlightsky" href={`/list/lessons?classId=${2}`}>Student&apos;s Lessons</Link>
+                        <Link className="p-3 rounded-md bg-SKlightpurple" href={`/list/teachers?classId=${2}`}>Student&apos;s Teachers</Link>
+                        <Link className="p-3 rounded-md bg-pink-50" href={`/list/exams?classId=${2}`}>Student&apos;s Exams</Link>
+                        <Link className="p-3 rounded-md bg-SKlightsky" href={`/list/assignments?classId=${2}`}>Student&apos;s Assignments</Link>
+                        <Link className="p-3 rounded-md bg-SKlightyellow" href={`/list/results?studentId=${"student2"}`}>Student&apos;s Results</Link>
                     </div>
                 </div>
                 <Performance />
--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -86,7 +86,7 @@ const SingleTeacherPage = () => {
                 </div> 
                 {/* bottom */}
                 <div className="mt-4  bg-white rounded-md p-4 h-[800px]">
-                    <h1>Teacher's Schedule</h1>
+                    <h1>Teacher&apos;s Schedule</h1>
                     <BigCalendar/>
                 </div>
             </div>
@@ -95,11 +95,11 @@ const SingleTeacherPage = () => {
                 <div className="bg-white p-4 rounded-md">
                     <h1 className="text-xl font-semibold">Shortcuts</h1>
                     <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-                        <Link className="p-3 rounded-md bg-SKlightsky" href={`/list/classes?supervisorId=${"teacher12"}`}>Teacher's Classes</Link>
-                        <Link className="p-3 rounded-md bg-SKlightpurple" href={`/list/students?teacherId=${"teacher2"}`}>Teacher's Students</Link>
-                        <Link className="p-3 rounded-md bg-SKlightyellow" href={`/list/lessons?teacherId=${"teacher12"}`}>Teacher's Lessons</Link>
-                        <Link className="p-3 rounded-md bg-pink-50" href={`/list/exams?teacherId=${"teacher12"}`}>Teacher's Exams</Link>
-                        <Link className="p-3 rounded-md bg-SKlightsky" href={`/list/assignments?teacherId=${"teacher12"}`}>Teacher's Assignments</Link>
+                        <Link className="p-3 rounded-md bg-SKlightsky" href={`/list/classes?supervisorId=${"teacher12"}`}>Teacher&apos;s Classes</Link>
+                        <Link className="p-3 rounded-md bg-SKlightpurple" href={`/list/students?teacherId=${"teacher2"}`}>Teacher&apos;s Students</Link>
+                        <Link className="p-3 rounded-md bg-SKlightyellow" href={`/list/lessons?teacherId=${"teacher12"}`}>Teacher&apos;s Lessons</Link>
+                        <Link className="p-3 rounded-md bg-pink-50" href={`/list/exams?teacherId=${"teacher12"}`}>Teacher&apos;s Exams</Link>
+                        <Link className="p-3 rounded-md bg-SKlightsky" href={`/list/assignments?teacherId=${"teacher12"}`}>Teacher&apos;s Assignments</Link>
                     </div>
                 </div>
                 <Performance/>
